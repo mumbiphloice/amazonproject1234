6452a3a558cd7dc3fd79d442ee67476b1a9c12a2
mumbiphloice <mumbiphloice@gmail.com>
2024-10-26 05:40:24 -0700

    Add files via upload

---
+++ amazon.project/scripts/data/cart.js
@@ -0,0 +1,51 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+  const matchingItem = cart.find((cartItem) => productId === cartItem.productId);
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  cart = cart.filter((cartItem) => cartItem.productId !== productId);
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  const matchingItem = cart.find((cartItem) => productId === cartItem.productId);
+
+  if (matchingItem) {
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
+  }
+}
+
