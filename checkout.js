4bad250523de46ea8f30ed4ac04e8362bd32f503
mumbiphloice <mumbiphloice@gmail.com>
2024-10-26 02:58:07 -0700

    Delete checkout.js

--- checkout.js
+++
@@ -1,9 +0,0 @@
-
-import { renderOrderSummary } from "./utilis/checkout/orderSummary.js";
-import { renderPaymentSummary } from "./utilis/checkout/paymentSummary.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-renderOrderSummary();
-renderPaymentSummary();
-
-});
